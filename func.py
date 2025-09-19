ef67a4a33435e7ddc7470faa29c1d4e06d700f45
FrozenGin <45907076+FrozenGin@users.noreply.github.com>
2025-09-19 10:59:36 +0200

    Adding testfiles

---
+++ func.py
@@ -0,0 +1,2 @@
+def func_plus(zahl1 : int, zahl2 : int):
+    return zahl1 + zahl2
---
+++ helloworld.py
@@ -0,0 +1,7 @@
+import func as funktionen
+print("Hello World")
+
+zahl1 = int(input("Bitte erste Zahl eingeben : "))
+zahl2 = int(input("Bitte zweite Zahl eingeben : "))
+
+print(f'{zahl1} + {zahl2} = {funktionen.func_plus(zahl1, zahl2)}')
